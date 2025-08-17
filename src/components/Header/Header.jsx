c9e3850e2608c938dd537c9599fc5f27888d3d27
Fagner Viana de Jesus <fagner.viana35.gmail.com>
2025-08-17 09:29:22 -0300

    teste

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleMenu = () => setOpen(!open);
+  const handleLink = () => setOpen(false);
+
   return (
     <header className="header">
       <div className="header-container">
@@ -10,14 +15,23 @@ const Header = () => {
             <img src="/images/logo.png" alt="Ágape Delícias Caseiras" />
           </a>
         </div>
-        <nav className="navigation">
+        <button
+          className={`hamburger${open ? ' open' : ''}`}
+          onClick={handleMenu}
+          aria-label="Abrir menu"
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <nav className={`navigation${open ? ' open' : ''}`}>
           <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#products">Produtos</a></li>
-            <li><a href="#about">Sobre Nós</a></li>
-            <li><a href="#partners">Parceiros</a></li>
-            <li><a href="#testimonials">Depoimentos</a></li>
-            <li><a href="#contact">Contato</a></li>
+            <li><a href="#home" onClick={handleLink}>Home</a></li>
+            <li><a href="#products" onClick={handleLink}>Produtos</a></li>
+            <li><a href="#about" onClick={handleLink}>Sobre Nós</a></li>
+            <li><a href="#partners" onClick={handleLink}>Parceiros</a></li>
+            <li><a href="#testimonials" onClick={handleLink}>Depoimentos</a></li>
+            <li><a href="#contact" onClick={handleLink}>Contato</a></li>
           </ul>
         </nav>
       </div>
